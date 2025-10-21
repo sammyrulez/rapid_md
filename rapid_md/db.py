6b6fa219ddb09da0914f134099391dcc9745b8fe
Sam <sam@dev.com>
2025-10-21 14:36:46 +0200

    formatting

--- rapid_md/db.py
+++ rapid_md/db.py
@@ -1,14 +1,12 @@
 import os
 from sqlalchemy.orm import Session, sessionmaker
 from sqlalchemy import create_engine
-from rapid_md.models import Base
+from typing import Generator
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-
-from typing import Generator
 
 def get_db() -> Generator[Session, None, None]:
     db = SessionLocal()
